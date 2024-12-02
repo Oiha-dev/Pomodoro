1cd3b50bcdb7ab3c9d634e0fac65bd474b0ef182
Gauthier Cenes <115953539+Oiha-dev@users.noreply.github.com>
2024-12-02 01:07:00 +0100

    feat(Timer): Added timer cycle - Added dynamic title based on timer state - Implemented timer cycle with 4 short breaks followed by 1 long break - Fixed 1-second delay issue when switching timer modes

--- src/routes/Timer.svelte
+++ src/routes/Timer.svelte
@@ -12,30 +12,47 @@
     let isRunning = false;
     let formattedTime = formatTime(runningTime);
     let interval;
+    let shortBreakCount = 0;
 
     onMount(() => {
         if (runningAtStart) startTimer();
     });
 
     function startTimer() {
-        if (isRunning) {
-            pause = !pause;
-            return;
+        if (isRunning && !pause) {
+            pause = true;
+            clearInterval(interval);
+        } else {
+            isRunning = true;
+            pause = false;
+            interval = setInterval(updateTimer, 1000);
         }
-        isRunning = true;
-        pause = false;
-        updateTimer();
-        interval = setInterval(updateTimer, 1000);
     }
 
     function updateTimer() {
         if (pause) return;
-        formattedTime = formatTime(runningTime);
         runningTime--;
+        formattedTime = formatTime(runningTime);
         if (runningTime < 0) {
             clearInterval(interval);
             isRunning = false;
+            handleTimerEnd();
         }
+    }
+
+    function handleTimerEnd() {
+        if (currentInitialTime === initialTime) {
+            if (shortBreakCount < 4) {
+                shortBreakCount++;
+                resetTimer(shortBreakTime);
+            } else {
+                shortBreakCount = 0;
+                resetTimer(longBreakTime);
+            }
+        } else {
+            resetTimer(initialTime);
+        }
+        startTimer();
     }
 
     function formatTime(seconds) {
@@ -49,16 +66,30 @@
         currentInitialTime = time;
         runningTime = time;
         formattedTime = formatTime(runningTime);
-        isRunning = false;
-        pause = false;
+        if (isRunning && !pause) {
+            interval = setInterval(updateTimer, 1000);
+        } else {
+            isRunning = false;
+            pause = false;
+        }
     }
 </script>
+
+<svelte:head>
+    {#if currentInitialTime === initialTime}
+        <title>Pomodoro {formattedTime}</title>
+    {:else if currentInitialTime === shortBreakTime}
+        <title>Short Break {formattedTime}</title>
+    {:else if currentInitialTime === longBreakTime}
+        <title>Long Break {formattedTime}</title>
+    {/if}
+</svelte:head>
 
 <div class="timer">
     <h1>{formattedTime}</h1>
     <button on:click={startTimer}>{isRunning && !pause ? 'Pause' : 'Start'}</button>
-    <button on:click={resetTimer(currentInitialTime)}>Reset</button>
-    <button on:click={resetTimer(initialTime)}>Pomodoro</button>
-    <button on:click={resetTimer(shortBreakTime)}>Short Break</button>
-    <button on:click={resetTimer(longBreakTime)}>Long Break</button>
+    <button on:click={() => resetTimer(currentInitialTime)}>Reset</button>
+    <button on:click={() => resetTimer(initialTime)}>Pomodoro</button>
+    <button on:click={() => resetTimer(shortBreakTime)}>Short Break</button>
+    <button on:click={() => resetTimer(longBreakTime)}>Long Break</button>
 </div>
